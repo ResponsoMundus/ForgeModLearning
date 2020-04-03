234f75d1830c05581a0a4411da662ae2139dc11d
ResponsoMundus <gamesluook_hi@126.com>
2020-04-03 19:23:20 -0400

    Reform Project

---
+++ src/main/java/mod/resmud/practicemod/PracticeMod.java
@@ -0,0 +1,8 @@
+package mod.resmud.practicemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(PracticeMod.MODID)
+public final class PracticeMod {
+    public static final String MODID= "praticemod";
+}
